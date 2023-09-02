0a84e862537c97d232eae3d0b18feefbfc3c4aa5
kaznatchey <123046298+kaznatchey@users.noreply.github.com>
2023-09-02 08:54:42 -0700

    Update ustanovka.py

--- ustanovka.py
+++ ustanovka.py
@@ -1,6 +1,15 @@
 import os
   
+install_httpd = """
+sudo yes y | yum install httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd.service
+sudo firewall-cmd --add-service=http --permanent
+sudo firewall-cmd --reload
+systemctl status httpd
+"""
+os.system(install_httpd)
 
-os.system("yes y | yum install httpd")
+# os.system("yes y | yum install httpd")
 
-os.system("yes y | yum install mysql")
+# os.system("yes y | yum install mysql")
