e4fcff17f969decf3190e4ac2b475ae3e186e760
kaznatchey <123046298+kaznatchey@users.noreply.github.com>
2023-09-09 08:37:51 -0700

    Update ustanovka.py

--- ustanovka.py
+++ ustanovka.py
@@ -7,6 +7,9 @@ sudo systemctl enable httpd.service
 sudo firewall-cmd --add-service=http --permanent
 sudo firewall-cmd --reload
 systemctl status httpd
+sudo chmod -R 775 /var/www/html/
+sudo chown -R cloud_user:cloud_user /var/www/html/
+ls -al /var/www/html/
 """
 os.system(install_httpd)
 
