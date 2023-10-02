d73a6a788b36a989b59f403273c7a5492169c0eb
Anna Chistyakova <achistya@users.noreply.github.com>
2023-10-01 18:24:00 -0700

    format date

--- button.js
+++ button.js
@@ -1,6 +1,7 @@
 const btnKick = document.querySelector("#kick");
 const attr = document.createAttribute("value");
-attr.value=Date();
+const dateObj = new Date();
+attr.value = dateObj.toISOString().substring(-1);
 console.log(btnKick);
 btnKick.setAttributeNode(attr);
 
