714ed3c3bd9a477b446b92d0974e00af62755053
kaznatchey <p.repnikova@gmail.com>
2023-11-04 19:38:06 -0700

    new

--- button.js
+++ button.js
@@ -1,12 +1,16 @@
 const btnKick = document.querySelector("#kick");
 const attr = document.createAttribute("value");
-const dateObj = new Date();
-attr.value = dateObj.toISOString().substring(0,23);
-console.log(btnKick);
-btnKick.setAttributeNode(attr);
+// const dateObj = new Date();
+// attr.value = dateObj.toISOString().substring(0,23);
+// console.log(btnKick);
+// btnKick.setAttributeNode(attr);
 
-// btnKick.addEventListener('click', function displayDate() {
+btnKick.addEventListener('click', function displayDate() {
     
-//     document.getElementById("B").innerHTML = Date();
+    // document.getElementById("B").innerHTML = Date();
+    const dateObj = new Date();
+    attr.value = dateObj.toISOString().substring(0,23);
+    console.log(btnKick);
+    btnKick.setAttributeNode(attr);
   
-// })
+})
