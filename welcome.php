d191022f64b885db26720b756b5961221ae67f46
Server <p.repnikova@gmail.com>
2023-11-11 19:38:00 +0000

    new line added

--- welcome.php
+++ welcome.php
@@ -4,8 +4,8 @@
 Kick time was recorded <?php echo $_POST["kicktime"]; ?>
 
 <?php
-$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-$txt = $_POST["kicktime"]+"\n";
+$myfile = fopen("newfile.txt", "a+") or die("Unable to open file!");
+$txt = $_POST["kicktime"]."\n";
 fwrite($myfile, $txt);
 fclose($myfile);
 echo "done";
