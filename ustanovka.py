b3c17f69423158c47bbe1d56cdebe0d2164a1293
Anna Chistyakova <achistya@users.noreply.github.com>
2024-02-24 10:44:13 -0800

    add razresheniya

--- ustanovka.py
+++ ustanovka.py
@@ -12,6 +12,10 @@ sudo chown -R cloud_user:cloud_user /var/www/html/
 sudo chown -R cloud_user:cloud_user /var/www/
 ls -al /var/www/html/
 sudo yes y | sudo yum install php
+chcon -R -t httpd_sys_content_t /var/www/html
+chcon -R -t httpd_sys_rw_content_t /var/www/html
+chmod 777 newfile.txt
+chmod 777 welcome.php
 """
 return_value = os.system(install_httpd)
 print(return_value)
